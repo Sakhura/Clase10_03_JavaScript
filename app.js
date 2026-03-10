48d08295b9c5325d17d401cfcbab699e82ae4f90
Sakhura <sabina.romero@gmail.com>
2026-03-10 19:46:11 -0300

    inicio

---
+++ app.js
@@ -0,0 +1,6 @@
+import { sumar, restar, multiplicar, dividir } from './math.js';
+
+console.log("Suma 10 + 5 = ", sumar(10 ,5));
+console.log("Resta 10 - 5 = ", restar(10 ,5));
+console.log("Multiplicación 10 * 5 = ", multiplicar(10 ,5));
+console.log("División 10 / 5 = ", dividir(10 ,5));
